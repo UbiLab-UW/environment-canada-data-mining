27e6009a8f20bbb4bcbb34f5eb1c4fd8d66234f4
Guilherme Abreu <guiii88@outlook.com>
2020-11-07 23:28:15 -0300

    update exemple code

--- exampleOfUse.py
+++ exampleOfUse.py
@@ -5,21 +5,20 @@ Created on Mon Nov  2 21:14:43 2020
 @author: guiii
 """
 
-from envcanlib import downloadData, to_sql, from_sql
+from envcanlib import getData, to_sql, from_sql
 import pandas as pd
 
 conn_string = "DRIVER={ODBC Driver 13 for SQL Server};\
-                                 SERVER=wwwwwww;\
-                                     DATABASE=zzzzzz;\
-                                         UID={XXXXX};\
-                                             PWD={YYYYY}"
+                                 SERVER=xxxxxxxx;\
+                                     DATABASE=xxxxx;\
+                                         UID={xxxxx};\
+                                             PWD={xxxxx}"
 
-downloadData(['155'], start=(2018,2), end=(2018,6))
+getData(['150','140'], start=(2018,2), end=(2018,6), saveFile=False, 
+        db_conn_string=conn_string, db_table_name='hourly')
+
 df = pd.read_csv('155.csv')
 
 to_sql(df, conn_string, 'new_function_test', if_exists='append')
 
-new_df = from_sql(conn_string,
-'''
-SELECT * FROM [dbo].[stations_inventory] WHERE Province = 'QUEBEC'
-''')
+new_df = from_sql(conn_string, 'stations_inventory')
